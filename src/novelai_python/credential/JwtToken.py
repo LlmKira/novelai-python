70c25c67122ef90c694b51479766fb20a9f4e63f
sudoskys <coldlando@hotmail.com>
2024-02-10 12:44:23 +0800

    :recycle: refactor(credential): update get_session method with headers parameter

--- src/novelai_python/credential/JwtToken.py
+++ src/novelai_python/credential/JwtToken.py
@@ -17,14 +17,21 @@ class JwtCredential(CredentialBase):
     jwt_token: SecretStr = Field(None, description="jwt token")
     _session: AsyncSession = None
 
-    async def get_session(self, timeout: int = 180):
+    async def get_session(self, timeout: int = 180, update_headers: dict = None):
+        if update_headers is None:
+            update_headers = {}
         if not self._session:
             self._session = AsyncSession(timeout=timeout, headers={
+                "Accept": "*/*",
+                "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+                "Accept-Encoding": "gzip, deflate, br",
                 "Authorization": f"Bearer {self.jwt_token.get_secret_value()}",
                 "Content-Type": "application/json",
                 "Origin": "https://novelai.net",
                 "Referer": "https://novelai.net/",
             }, impersonate="chrome110")
+        assert isinstance(update_headers, dict), "update_headers must be a dict"
+        self._session.headers.update(update_headers)
         return self._session
 
     @field_validator('jwt_token')
--- src/novelai_python/credential/UserAuth.py
+++ src/novelai_python/credential/UserAuth.py
@@ -21,16 +21,22 @@ class LoginCredential(CredentialBase):
     _session: Optional[AsyncSession] = None
     _update_at: Optional[int] = None
 
-    async def get_session(self, timeout: int = 180):
+    async def get_session(self, timeout: int = 180, update_headers: dict = None):
+        if update_headers is None:
+            update_headers = {}
         # 30 天有效期
         if not self._session or int(time.time()) - self._update_at > 29 * 24 * 60 * 60:
             from ..sdk import Login
             resp = await Login.build(user_name=self.username, password=self.password.get_secret_value()).request()
             self._session = AsyncSession(timeout=timeout, headers={
+                "Accept": "*/*",
+                "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+                "Accept-Encoding": "gzip, deflate, br",
                 "Authorization": f"Bearer {resp.accessToken}",
                 "Content-Type": "application/json",
                 "Origin": "https://novelai.net",
                 "Referer": "https://novelai.net/",
             }, impersonate="chrome110")
             self._update_at = int(time.time())
+        self._session.headers.update(update_headers)
         return self._session
