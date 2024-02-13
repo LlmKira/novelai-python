9dbe85be2c72794df1f7541484feab35032f1e96
sudoskys <coldlando@hotmail.com>
2024-02-13 12:16:52 +0800

    :sparkles: feat(playground): add upscale.py
    Added `upscale.py` to the `playground` directory. This file contains a demo
    that demonstrates how to upscale an image using the NovelAI Python SDK.

---
+++ playground/upscale.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/2/13 上午11:58
+# @Author  : sudoskys
+# @File    : upscale.py
+# @Software: PyCharm
+# -*- coding: utf-8 -*-
+# @Time    : 2024/2/14 下午5:20
+# @Author  : sudoskys
+# @File    : upscale_demo.py
+# @Software: PyCharm
+# To run the demo, you need an event loop, for instance by using asyncio
+import asyncio
+import os
+
+from dotenv import load_dotenv
+from pydantic import SecretStr
+
+from novelai_python import APIError, Upscale
+from novelai_python import UpscaleResp, JwtCredential
+from novelai_python.sdk.ai.generate_image import Action
+from novelai_python.utils.useful import enum_to_list
+
+load_dotenv()
+
+token = None
+jwt = os.getenv("NOVELAI_JWT") or token
+
+
+async def main():
+    globe_s = JwtCredential(jwt_token=SecretStr(jwt))
+    if not os.path.exists("generate_image.png"):
+        raise FileNotFoundError("generate_image.png not found")
+    with open("generate_image.png", "rb") as f:
+        data = f.read()
+    try:
+        print(f"Action List:{enum_to_list(Action)}")
+        upscale = Upscale(image=data)  # Auto detect image size | base64
+
+        _res = await upscale.request(
+            session=globe_s, remove_sign=True
+        )
+    except APIError as e:
+        print(e.response)
+        return
+
+    # Meta
+    _res: UpscaleResp
+    print(_res.meta.endpoint)
+    file = _res.files
+    with open("upscale.py.png", "wb") as f:
+        f.write(file[1])
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
