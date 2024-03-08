7a2ee5ac0ab8004a2b663f655cb6df7f6b374519
sudoskys <coldlando@hotmail.com>
2024-03-09 00:46:15 +0800

    ✨ feat: refactor generate_image_img2img.py and generate_image.py
    - Refactored generate_image_img2img.py and generate_image.py
    - Updated prompt parameter in generate function
    - Improved error handling and file writing logic

--- playground/generate_image.py
+++ playground/generate_image.py
@@ -5,7 +5,7 @@
 # @Software: PyCharm
 import asyncio
 import os
-
+import pathlib
 from dotenv import load_dotenv
 from pydantic import SecretStr
 
@@ -14,41 +14,42 @@ from novelai_python import GenerateImageInfer, ImageGenerateResp, JwtCredential
 from novelai_python.sdk.ai.generate_image import Action, Sampler
 from novelai_python.utils.useful import enum_to_list
 
-load_dotenv()
 
-enhance = "year 2023,dynamic angle, best quality, amazing quality, very aesthetic, absurdres"
-token = None
-jwt = os.getenv("NOVELAI_JWT") or token
-
-
-async def main():
-    globe_s = JwtCredential(jwt_token=SecretStr(jwt))
-    _res = await Login.build(user_name=os.getenv("NOVELAI_USER"), password=os.getenv("NOVELAI_PASS")
-                             ).request()
+async def generate(prompt="1girl, year 2023, dynamic angle, best quality, amazing quality, very aesthetic, absurdres"):
+    jwt = os.getenv("NOVELAI_JWT", None)
+    if jwt is None:
+        raise ValueError("NOVELAI_JWT is not set in `.env` file, please create one and set it")
+    credential = JwtCredential(jwt_token=SecretStr(jwt))
+    """Or you can use the login credential to get the renewable jwt token"""
+    _login_credential = Login.build(
+        user_name=os.getenv("NOVELAI_USER"),
+        password=os.getenv("NOVELAI_PASS")
+    )
+    # await _login_credential.request()
+    print(f"Action List:{enum_to_list(Action)}")
     try:
-        print(f"Action List:{enum_to_list(Action)}")
-        gen = GenerateImageInfer.build(
-            prompt=f"1girl, winter, jacket, sfw, angel, flower,{enhance}",
+        agent = GenerateImageInfer.build(
+            prompt=prompt,
             action=Action.GENERATE,
-            sampler=Sampler.DDIM_V3
+            sampler=Sampler.K_DPMPP_SDE,
+            qualityToggle=True,
         )
-        cost = gen.calculate_cost(is_opus=True)
-        print(f"charge: {cost} if you are vip3")
-        print(f"charge: {gen.calculate_cost(is_opus=False)} if you are not vip3")
-        _res = await gen.request(
-            session=globe_s, remove_sign=True
+        print(f"charge: {agent.calculate_cost(is_opus=True)} if you are vip3")
+        print(f"charge: {agent.calculate_cost(is_opus=False)} if you are not vip3")
+        result = await agent.request(
+            session=credential, remove_sign=True
         )
     except APIError as e:
-        print(str(e))
-        print(e.response)
-        return
-
+        print(f"Error: {e.message}")
+        return None
+    else:
+        print(f"Meta: {result.meta}")
     _res: ImageGenerateResp
-    print(_res.meta)
-    file = _res.files[0]
-    with open("generate_image.png", "wb") as f:
+    file = result.files[0]
+    with open(f"{pathlib.Path(__file__).stem}.png", "wb") as f:
         f.write(file[1])
 
 
+load_dotenv()
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+loop.run_until_complete(generate())
