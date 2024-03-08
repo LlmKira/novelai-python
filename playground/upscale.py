2102ba3a67ead721e430ae7652c7a94d0b0ef644
sudoskys <coldlando@hotmail.com>
2024-03-09 00:45:47 +0800

    :recycle: chore(playground): refactor upscale.py with new generate function

--- playground/upscale.py
+++ playground/upscale.py
@@ -11,6 +11,7 @@
 # To run the demo, you need an event loop, for instance by using asyncio
 import asyncio
 import os
+from pathlib import Path
 
 from dotenv import load_dotenv
 from pydantic import SecretStr
@@ -26,11 +27,13 @@ token = None
 jwt = os.getenv("NOVELAI_JWT") or token
 
 
-async def main():
+async def generate(
+        image_path="static_refer.png"
+):
     globe_s = JwtCredential(jwt_token=SecretStr(jwt))
-    if not os.path.exists("generate_image.png"):
-        raise FileNotFoundError("generate_image.png not found")
-    with open("generate_image.png", "rb") as f:
+    if not os.path.exists(image_path):
+        raise FileNotFoundError(f"{image_path} not found")
+    with open(image_path, "rb") as f:
         data = f.read()
     try:
         print(f"Action List:{enum_to_list(Action)}")
@@ -47,9 +50,9 @@ async def main():
     _res: UpscaleResp
     print(_res.meta.endpoint)
     file = _res.files
-    with open("upscale.py.png", "wb") as f:
+    with open(f"{Path(__file__).stem}.png", "wb") as f:
         f.write(file[1])
 
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+loop.run_until_complete(generate())
