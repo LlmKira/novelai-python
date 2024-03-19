111fb86aa53622934a235f40c767795f163f2803
sudoskys <coldlando@hotmail.com>
2024-03-19 22:19:38 +0800

    :arrow_up: chore(pyproject): bump version to 0.4.3

--- src/novelai_python/sdk/ai/generate_image/__init__.py
+++ src/novelai_python/sdk/ai/generate_image/__init__.py
@@ -161,7 +161,7 @@ class GenerateImageInfer(ApiBaseModel):
                 image: Union[str, bytes],
                 width: int = 448,
                 height: int = 448,
-                transparency: bool = True
+                transparency: bool = False
         ):
 
             """
