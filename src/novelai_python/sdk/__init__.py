f31af0dc5f539b804733cef21579d37d1cd16b77
sudoskys <coldlando@hotmail.com>
2024-03-31 18:44:27 +0800

    ✨ feat: add VoiceGenerate and VoiceResponse classes in sdk
    Add VoiceGenerate and VoiceResponse classes in sdk for voice generation.

--- src/novelai_python/sdk/__init__.py
+++ src/novelai_python/sdk/__init__.py
@@ -6,6 +6,7 @@
 
 from .ai.generate_image import GenerateImageInfer, ImageGenerateResp  # noqa 401
 from .ai.generate_image.suggest_tags import SuggestTags, SuggestTagsResp  # noqa 401
+from .ai.generate_voice import VoiceGenerate, VoiceResponse  # noqa 401
 from .ai.upscale import Upscale, UpscaleResp  # noqa 401
 from .user.information import Information, InformationResp  # noqa 401
 from .user.login import Login, LoginResp  # noqa 401
