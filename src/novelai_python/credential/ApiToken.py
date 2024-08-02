06d1f9aedc2b8336b38f17e48cc787e0cace5a04
sudoskys <coldlando@hotmail.com>
2024-08-02 16:03:02 +0800

    🐛 fix: Correct variable name in ApiToken and JwtToken classes
    - Fixed the variable name from JwtCredential to ApiCredential in the ApiToken
      class.
    - Updated the variable name in the JwtToken class for consistency.

--- src/novelai_python/credential/ApiToken.py
+++ src/novelai_python/credential/ApiToken.py
@@ -3,6 +3,7 @@
 # @Author  : sudoskys
 # @File    : ApiToken.py
 # @Software: PyCharm
+
 from curl_cffi.requests import AsyncSession
 from loguru import logger
 from pydantic import SecretStr, Field, field_validator
@@ -12,27 +13,26 @@ from ._base import CredentialBase, FAKE_UA
 
 class ApiCredential(CredentialBase):
     """
-    JwtCredential is the base class for all credential.
+    ApiCredential is the base class for all credential.
     """
     api_token: SecretStr = Field(None, description="api token")
-    _session: AsyncSession = None
 
     async def get_session(self, timeout: int = 180, update_headers: dict = None):
-        if update_headers is None:
-            update_headers = {}
-        if not self._session:
-            self._session = AsyncSession(timeout=timeout, headers={
-                "Accept": "*/*",
-                "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-                "Accept-Encoding": "gzip, deflate, br",
-                "User-Agent": FAKE_UA.edge,
-                "Authorization": f"Bearer {self.api_token.get_secret_value()}",
-                "Content-Type": "application/json",
-                "Origin": "https://novelai.net",
-                "Referer": "https://novelai.net/",
-            }, impersonate="edge101")
-        self._session.headers.update(update_headers)
-        return self._session
+        headers = {
+            "Accept": "*/*",
+            "Accept-Encoding": "gzip, deflate, br",
+            "User-Agent": FAKE_UA.edge,
+            "Authorization": f"Bearer {self.api_token.get_secret_value()}",
+            "Content-Type": "application/json",
+            "Origin": "https://novelai.net",
+            "Referer": "https://novelai.net/",
+        }
+
+        if update_headers:
+            assert isinstance(update_headers, dict), "update_headers must be a dict"
+            headers.update(update_headers)
+
+        return AsyncSession(timeout=timeout, headers=headers, impersonate="edge101")
 
     @field_validator('api_token')
     def check_api_token(cls, v: SecretStr):
--- src/novelai_python/credential/JwtToken.py
+++ src/novelai_python/credential/JwtToken.py
@@ -15,25 +15,23 @@ class JwtCredential(CredentialBase):
     JwtCredential is the base class for all credential.
     """
     jwt_token: SecretStr = Field(None, description="jwt token")
-    _session: AsyncSession = None
 
     async def get_session(self, timeout: int = 180, update_headers: dict = None):
-        if update_headers is None:
-            update_headers = {}
-        if not self._session:
-            self._session = AsyncSession(timeout=timeout, headers={
-                "Accept": "*/*",
-                "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-                "User-Agent": FAKE_UA.edge,
-                "Accept-Encoding": "gzip, deflate, br",
-                "Authorization": f"Bearer {self.jwt_token.get_secret_value()}",
-                "Content-Type": "application/json",
-                "Origin": "https://novelai.net",
-                "Referer": "https://novelai.net/",
-            }, impersonate="edge101")
-        assert isinstance(update_headers, dict), "update_headers must be a dict"
-        self._session.headers.update(update_headers)
-        return self._session
+        headers = {
+            "Accept": "*/*",
+            "User-Agent": FAKE_UA.edge,
+            "Accept-Encoding": "gzip, deflate, br",
+            "Authorization": f"Bearer {self.jwt_token.get_secret_value()}",
+            "Content-Type": "application/json",
+            "Origin": "https://novelai.net",
+            "Referer": "https://novelai.net/",
+        }
+
+        if update_headers:
+            assert isinstance(update_headers, dict), "update_headers must be a dict"
+            headers.update(update_headers)
+
+        return AsyncSession(timeout=timeout, headers=headers, impersonate="edge101")
 
     @field_validator('jwt_token')
     def check_jwt_token(cls, v: SecretStr):
